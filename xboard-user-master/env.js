66a125326537f4c84edd1abd396a75d915696af9
shanzixiaojin485-lab <shanzixiaojin485@gmail.com>
2026-03-02 21:48:29 +0800

    Add files via upload

---
+++ xboard-user-master/env.js
@@ -0,0 +1,19 @@
+// API地址
+window.routerBase = 'https://www.wyun.im'
+window.settings = {
+  // 站点名称
+  title: 'LingSpeed',
+  // 站点描述
+  description: 'LingSpeed超快！超稳定！',
+  assets_path: '/assets',
+  // 主题色
+  theme: {
+    color: 'default', //可选default、blue、black、、darkblue
+  },
+  // 版本号
+  version: '0.1.1-dev',
+  // 登陆背景
+  background_url: 'https://api.neix.in/random/',
+  // 站点LOGO
+  logo: '/log/logo.jpg',
+}
